49e018036640bf905acf945b54aadf1404182dfa
verbatim144 <vlad061998@gmail.com>
2019-04-03 00:08:58 +0200

    Add Neighbor model to the project

---
+++ src/main/java/com/verbatim/neighbor/model/Neighbor.java
@@ -0,0 +1,91 @@
+package com.verbatim.neighbor.model;
+
+import javax.persistence.*;
+
+@Entity // to indicate that it is a JPA(Java Persistence API) entity
+@Table(name= "neighbor")  // specify the details of the table
+public class Neighbor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "country")
+    private Integer country;
+
+    @Column(name = "city")
+    private String city;
+
+    public Neighbor(String name, String surname, Integer age, Integer country, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.country = country;
+        this.city = city;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getCountry() {
+        return country;
+    }
+
+    public void setCountry(Integer country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Neighbor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", country=" + country +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
