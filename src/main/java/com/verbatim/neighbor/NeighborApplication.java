950aadf6c719ac6fea071cb706e94eee2bf903ef
verbatim144 <vlad061998@gmail.com>
2019-04-02 21:52:44 +0200

    Create new project

---
+++ src/main/java/com/verbatim/neighbor/NeighborApplication.java
@@ -0,0 +1,13 @@
+package com.verbatim.neighbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborApplication.class, args);
+	}
+
+}
