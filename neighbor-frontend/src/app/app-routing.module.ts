493964b4a70381f0f0ed9d3b4b4e1975674cb2c5
verbatim144 <vlad061998@gmail.com>
2019-04-06 00:10:14 +0200

    Create page routing

---
+++ neighbor-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {RouterModule, Routes} from '@angular/router';
+import {NeighborListComponent} from './neighbor-list/neighbor-list.component';
+import {CreateNeighborComponent} from './create-neighbor/create-neighbor.component';
+import {NgModule} from '@angular/core';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'neighbor', pathMatch: 'full'},
+  {path: 'neighbor', component: NeighborListComponent},
+  {path: 'add', component: CreateNeighborComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
