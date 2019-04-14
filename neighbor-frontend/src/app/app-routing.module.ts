90246456dc69b90764a34da135d44acfdfb38569
verbatim144 <vlad061998@gmail.com>
2019-04-14 23:51:20 +0200

    Add spring security to pom.xml

--- neighbor-frontend/src/app/app-routing.module.ts
+++ neighbor-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {NeighborListComponent} from './neighbor-list/neighbor-list.component';
 import {CreateNeighborComponent} from './create-neighbor/create-neighbor.component';
 import {NgModule} from '@angular/core';
+import {UserService} from './app.service';
 
 const routes: Routes = [
   {path: '', redirectTo: 'neighbor', pathMatch: 'full'},
   {path: 'neighbor', component: NeighborListComponent},
-  {path: 'add', component: CreateNeighborComponent}
+  {path: 'add', component: CreateNeighborComponent},
+  {path: 'users', component: UserService}
 ];
 
 @NgModule({
--- src/main/java/com/verbatim/neighbor/controller/NeighborController.java
+++ src/main/java/com/verbatim/neighbor/controller/NeighborController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class NeighborController {
